c3253aa088e7a3391c1d935c3f4aeb3dd98effbd
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-11-14 10:49:48 +0100

    Spawn elm-reactor

--- bin/elm-live.js
+++ bin/elm-live.js
@@ -2,10 +2,10 @@
 
 const Gaze = require('gaze').Gaze;
 const path = require('path');
+const spawnSync = require('child_process').spawnSync;
 
 // Process info
 const flags = require('minimist')(process.argv.slice(2), {boolean: true});
-const files = flags._;
 
 // Print usage
 if (flags.h) process.stdout.write(require('./help/usage'));
@@ -18,8 +18,29 @@ if (flags.help) process.stdout.write([
 // Exit early
 if (flags.h || flags.help) process.exit(0);
 
-// Watch
+// Spawn elm-reactor
+const reactorArgs = flags._;
 const cwd = process.cwd();
+const reactor = spawnSync('elm-reactor', {stdio: 'inherit'});
+if (reactor.error) {
+  if (reactor.error.code = 'ENOENT') {
+    process.stderr.write(
+`elm-live: I can’t find the command \`elm-reactor\`! Looks like elm-platform
+  isn’t installed. Make sure you’ve followed the steps at http://npm.im/elm
+  and that you can call \`elm-reactor\` on your command line.
+
+  If that fails, have a look at open issues:
+  https://github.com/tomekwi/elm-live/issues .
+
+`
+    );
+    process.exit(1);
+  } else {
+    throw reactor.error;
+  }
+};
+
+// Watch
 const gaze = new Gaze('**/*.elm');
 
 gaze.on('all', (_, filePath) => {
