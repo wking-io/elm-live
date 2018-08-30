8f0ea71b10cbe33bd64dc19383e8b66ba64838c4
Will King <contact@wking.io>
2018-08-30 10:32:15 -0500

    Removing unused deps

--- bin/elm-live.js
+++ bin/elm-live.js
@@ -6,7 +6,7 @@ const chalk = require('chalk')
 program
   .version(require('../package.json').version)
   .arguments('<elm-main>')
-  .usage(`${chalk.magenta('<elm-main>')} [options]`)
+  .usage(`${chalk.magenta('<elm-main>')} [options] [--] [elm make options]`)
   .option('-p, --port [port]', 'The port to bind to.', parseInt, 8000)
   .option('-e, --path-to-elm [path-to-elm]', 'An absolute or relative path to elm. If you’ve installed elm locally with npm you’ll want to set this to `node_modules/.bin/elm`.', 'elm')
   .option('-h, --host [host]', 'Set the host interface to attach the server to.', 'localhost')
@@ -17,7 +17,7 @@ program
   .option('-b, --before-build [before-build]', `Run EXECUTABLE before every rebuild. This way you can easily use other tools like ${chalk.cyan.underline('elm-css')} or ${chalk.cyan.underline('browserify')} in your workflow.`)
   .option('-a, --after-build [after-build]', `Just like ${chalk.cyan.underline('--before-build')}, but runs after ${chalk.cyan.underline('elm make')}.`)
   .on('--help', () => {
-    console.log(`    Only ${chalk.magenta('<elm-main>')} is required.`)
+    console.log(`    Only ${chalk.magenta('<elm-main>')} is required. If you want to pass on specific options to ${chalk.cyan.underline('elm make')} make sure they are passed after the -- in the command.`)
     console.log()
     console.log(
       `    If you have any problems, do not hesitate to file an issue:`
