7690f49716aa2757c3220e75aced9f835c34ece4
Will King <contact@wking.io>
2019-06-20 10:44:11 -0500

    AAdding weebsite

---
+++ www/gatsby-config.js
@@ -0,0 +1,49 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
+module.exports = {
+  siteMetadata: {
+    title: 'elm-live | Live reloading server for Elm development'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'elm-live-docs',
+        short_name: 'elm-live',
+        start_url: '/',
+        background_color: '#FFFFFF',
+        theme_color: '#FFFFFF',
+        display: 'minimal-ui',
+        icon: 'src/images/elm-live-icon.png' // This path is relative to the root of the site.
+      }
+    },
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'Github',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        headers: {
+          Authorization: `bearer ${process.env.GITHUB_TOKEN}`
+        },
+        fetchOptions: {}
+      }
+    },
+    'gatsby-plugin-postcss'
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ]
+}
