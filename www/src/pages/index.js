96b023444e4ccb1c4105291526b5338811e6e324
Will King <contact@wking.io>
2019-07-04 08:45:27 -0500

    Finished up desktop

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -18,7 +18,6 @@ function getStats (data) {
   const version = safePath(['repository', 'releases', 'nodes'], data)
     .map(a => a[0])
     .chain(safePath(['tag', 'name']))
-    .map(v => v.substring(1))
 
   return Either
     .of(a => b => c => [{ name: 'Forks', value: a }, { name: 'Stars', value: b }, { name: 'Version', value: c }])
@@ -30,11 +29,11 @@ function getStats (data) {
 const getRepoUrl = safePath(['repository', 'url'])
 
 const renderStats = stats => (
-  <ul>
+  <ul className="flex border-t-2 border-black">
     {stats.map(({ name, value }) => (
-      <li key={`${name}-${value}`} >
-        <p>{name}</p>
-        <p>{value}</p>
+      <li className="stat flex-1 pt-4 mr-4" key={`${name}-${value}`} >
+        <p className="text-sm leading-none mb-1">{name}</p>
+        <p className="text-4xl font-bold leading-none">{value}</p>
       </li>
     ))}
   </ul>
@@ -43,38 +42,62 @@ const renderStats = stats => (
 const IndexPage = ({ data }) => {
 
   const stats = getStats(data.github)
-  const repoUrl = getRepoUrl(data.github)
+  const repoUrl = getRepoUrl(data.github).getOrElse('https://github.com/wking-io/elm-live')
 
   return (
-    <div>
+    <div className="font-sans text-black flex">
       <SEO title="elm-live | Live reloading server for Elm development" />
-      <header>
-        <h1><span>elm-live</span><LogoIcon /></h1>
-        <div>
-          <h2>A flexible dev server for Elm. Live reload included.</h2>
-          <p>A thin wrapper around elm make, elm-live is a dev server that gives you a few extra convenience features during development. Features include pushstate for SPA development, proxies, and more. Usage and API documentation is below. Check out <a href="#getting-started">how to get started</a> or jump straight to the <a href="#documentation">API Documentation</a>.</p>
+      <header className="p-8 w-1/3 flex-shrink-0 flex flex-col space-between h-screen fixed z-10 bg-white">
+        <h1 className="flex items-center font-bold mb-12">
+          <span className="h-8 mr-2"><LogoIcon /></span>
+          <span className="leading-none">elm-live</span>
+        </h1>
+        <div className="flex-1">
+          <h2 className="text-2xl mb-4 font-bold leading-tight">A flexible dev server for Elm. Live reload included.</h2>
+          <p>A thin wrapper around elm make, elm-live is a dev server that gives you a few extra convenience features during development. Features include pushstate for SPA development, proxies, and more. Usage and API documentation is below. Check out <a className="link" href="#getting-started">how to get started</a> or jump straight to the <a className="link" href="#documentation">API Documentation</a>.</p>
         </div>
         <div>
-          <p><strong>Would you be willing to answer a few questions to help improve development in elm?</strong></p>
-          <Link>Take the quick survey</Link>
+          <p className="mb-4"><strong>Would you be willing to answer a few questions to help improve development in elm?</strong></p>
+          <Link to="/surveys/2019" className="btn btn--primary block mb-6">Take the quick survey</Link>
         </div>
         {stats.fold(console.log, renderStats)}
       </header>
-      <nav>
-        <ul>
-          <li><a href="#getting-started">Getting Started</a></li>
-          <li><a href="#documentation">Documentation</a></li>
-        </ul>
-        <ul>
-          <li><a href="https://wking.io"><WebIcon/></a></li>
-          <li><a href={repoUrl} ><GithubIcon/></a></li>
-          <li><button><MenuIcon/></button></li>
-        </ul>
+      <nav className="fixed w-2/3 right-0 nav bg-grey-light px-12 z-10">
+        <div className="flex pt-8 border-b-2 border-black">
+          <ul className="flex flex-1">
+            <li className="text-sm"><a className="block mr-6 pb-3 border-b-2 border-grey-light hover:border-black" href="#getting-started">Getting Started</a></li>
+            <li className="text-sm"><a className="block pb-3 border-b-2 border-grey-light hover:border-black" href="#documentation">Documentation</a></li>
+          </ul>
+          <ul className="flex">
+            <li className="h-4 w-auto mr-4"><a href="https://wking.io"><WebIcon/></a></li>
+            <li className="h-4 w-auto mr-4"><a href={repoUrl} ><GithubIcon/></a></li>
+            <li className="h-4 w-auto"><button className="w-4"><MenuIcon/></button></li>
+          </ul>
+        </div>
       </nav>
-      <main>
-        <h2 id="geting-started"># Getting Started</h2>
-        <pre>elm-live &gt;elm-file&lt; [other-elm-files...] [flags] [--] [elm make flags]</pre>
-        <p>Although all the flags are broken down in the documentation below I want to cover the different parts of the command you see above so that there is nothing left you need to assume or guess at:</p>
+      <main className="content relative bg-grey-light pt-32 px-12 w-2/3 ml-auto">
+        <h2 className="text-3xl font-bold mb-6" id="geting-started"># Getting Started</h2>
+        <pre className="code bg-grey leading-none text-sm mb-6">
+          <code>elm-live &lt;elm-file&gt; [other-elm-files...] [flags] [--] [elm make flags]</code>
+        </pre>
+        <p className="leading-relaxed mb-8">Although all the flags are broken down in the documentation below I want to cover the different parts of the command you see above so that there is nothing left you need to assume or guess at:</p>
+        <h3 className="text-2xl font-bold mb-4" id="geting-started-elm-live"># elm-live</h3>
+        <p className="leading-relaxed mb-8">This is the name of the binary that is installed either to your local or global path.</p>
+        <h3 className="text-2xl font-bold mb-4" id="geting-started-elm-live"># &lt;elm-file&gt;</h3>
+        <p className="leading-relaxed mb-8">This represents the one required argument and is the path to the Elm file you want to compile. This file path gets passed directly to elm make.</p>
+        <h3 className="text-2xl font-bold mb-4" id="geting-started-elm-live"># [other-elm-files...]</h3>
+        <p className="leading-relaxed mb-8">This represents the potentially infinite, but completely optional other Elm files you want to compile, E.g. <code className="font-mono">elm-live src/Main.elm src/Widget.elm src/Tool.elm</code>.</p>
+        <h3 className="text-2xl font-bold mb-4" id="geting-started-elm-live"># [--] [elm make flags]</h3>
+        <p className="leading-relaxed mb-8">This represents the optional <code>elm-live</code> specific flags that help manage how the server is setup. See below for docs on how these are used.</p>
+        <h3 className="text-2xl font-bold mb-4" id="geting-started-elm-live"># [--] [elm make flags]</h3>
+        <p className="leading-relaxed mb-8">These come as a package deal. Both are optional, but you cannot have one without the other. The <code>--</code> allows us to capture all flags after it into a single bucket. We use it to group the <code>elm make</code> flags and pass them right into the command. This keeps all <code>elm make</code> flags future proof. If Elm releases new flags for <code>elm make</code> they will be immediately avaialble for use in <code>elm-live</code> without needing to upgrade since we do not depend on parsing the flags in our source code. See below for docs on what <code>elm make</code> flags are available and how they are used.</p>
+        <h2 className="text-3xl font-bold mb-4" id="documentation"># Flags</h2>
+        <h3 className="text-2xl font-bold mb-6" id="geting-started-elm-live"># --port, -p</h3>
+        <pre className="code bg-grey leading-none text-sm mb-6">
+          <code>elm-live src/Main.elm --port=1234</code>
+        </pre>
+        <p className="mb-4"><strong>Default: 4000</strong></p>
+        <p className="leading-relaxed mb-8">Set the port that the elm-live server is listening for requests at. If the default port used by elm-live is already in use this flag gives you a backdoor to pass in a port that is not in use. In the example above the url that your elm project would be available at would be <code>http://localhost::1234</code>.</p>
       </main>
     </div>
   )
--- www/tailwind.config.js
+++ www/tailwind.config.js
@@ -1,5 +1,19 @@
+const colors = {
+  black: "#0D1F2D",
+  white: "#ffffff",
+  primary: "#F77F00",
+  "primary-dark": "#EF6300",
+  "primary-light": "#FFA01C",
+  grey: "#D3DEE2",
+  "grey-light": "#F3F4F5",
+}
 module.exports = {
   theme: {
+    fontFamily: {
+      sans: ["Fira Sans", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      mono: ["Fira Mono", "Menlo", "Monaco", "Consolas", "Liberation Mono", "Courier New", "monospace"]
+    },
+    colors,
     extend: {}
   },
   variants: {},
