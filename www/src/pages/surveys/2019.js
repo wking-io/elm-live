6f47486557b40a1ad10b2a65ecf3e65b568c3965
Will King <contact@wking.io>
2019-07-16 13:45:37 -0500

    Quick update

--- www/src/pages/surveys/2019.js
+++ www/src/pages/surveys/2019.js
@@ -2,14 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import SEO from "../../components/seo"
+import Layout from "../../components/layout"
 
 const SecondPage = () => (
-  <div>
-    <SEO title="elm-live 2019 survey" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
+  <Layout>
+    <div>
+      <SEO title="elm-live 2019 survey" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
+  </Layout>
 )
 
 export default SecondPage
